66b3fd1bdb79c6448d00e38a39149c4c52561ad5
OHHAKO <l36h7129@gmail.com>
2020-04-20 17:54:26 +0900

    Update bj 1010 solution by using BigInteger in source code

--- source/ohhako/boj/bj_1010.java
+++ source/ohhako/boj/bj_1010.java
@@ -1,28 +1,26 @@
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.math.BigInteger;
 
-public class Main{
-    public static void makeBridge(int n,int m){
-        int answer=0;
-        int parent=1;
-        int child=1;
-        for(int i=0 ; i<n-1 ; i++){
-            parent = parent*(m-i);
-        }
-        parent = parent*(m-n+1);
-        
-        for(int i=1 ; i<=n ; i++)
-            child = child*i;
-        System.out.println(parent/child);
-    }
-    
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        for(int i=0 ; i<t ; i++){
-            String[] cmd = br.readLine().split(" ");
-            makeBridge(Integer.parseInt(cmd[0]),Integer.parseInt(cmd[1]));
-        }
-    }
+public class Main {
+	public static void makeBridge(int n, int m) {
+		BigInteger parent = BigInteger.ONE;
+		BigInteger child = BigInteger.ONE;
+		
+		for (int i = 1; i <= n ; i++) {
+			parent = parent.multiply(BigInteger.valueOf(m-i+1));
+			child = child.multiply(BigInteger.valueOf(i));
+		}
+		System.out.println(parent.divide(child));
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		for (int i = 0; i < t; i++) {
+			String[] cmd = br.readLine().split(" ");
+			makeBridge(Integer.parseInt(cmd[0]), Integer.parseInt(cmd[1]));
+		}
+	}
 }
